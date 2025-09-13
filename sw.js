0fa12a851f081690d814d3f754fd057b2f3e2bd5
Sahil Kalra <Kalrakreations786@gmail.com>
2025-09-13 18:04:02 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'visitor-form-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/form-style.css',
+  '/form-script.js',
+  '/manifest.json',
+  '/icons/icon192.png',
+  '/icons/icon512.png'
+];
+
+// Install event - caching all assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event - cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => 
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            console.log('Deleting old cache:', name);
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event - serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
